e9cc846d776a4737b1c3b6ab42b066e01452bb62
yifat natovich <ynatovich@gmail.com>
2020-05-11 13:17:23 +0300

    copy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage.component';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage/>
+      </div>
+    );
+  }
+}
+export default App;
